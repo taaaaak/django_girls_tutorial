30ba5131e34a4e551d588d06d5b657ad877109c4
taaaaak <a3ae10511@gmail.com>
2019-02-04 21:29:06 +0900

    サーバが正常起動するところまで戻す

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('blog.urls')),
+    # path('', include('blog.urls')),
     path('admin/', admin.site.urls),
     # path('', RedirectView.as_view(url='/blog/')),
 ]
